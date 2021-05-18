10e795a2b47a67d3eddf70abd852e0c18bb9640e
LAPTOP-SQTE162T\bojan <bojanacvetko19@gmail.com>
2021-05-18 10:37:56 +0200

    Maintence Monitor commiten

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,29 @@
+import com.sun.net.httpserver.HttpServer;
+import org.glassfish.jersey.jdkhttp.JdkHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+
+import java.io.IOException;
+import java.net.URI;
+
 public class Main {
+
     public static void main(String[] args) {
         System.out.println(GetMessage());
     }
+    ResourceConfig rc = new ResourceConfig()
+            .packages("at.gruppeJ.swlcm.MaintenceMonitor.controller");
+    HttpServer server = JdkHttpServerFactory.createHttpServer(URI.create("http://localhost:8080/"), rc);
+
+
+        try {
+        System.in.read();
+    } catch (IOException e) {
+        e.printStackTrace();
+    }
+
+        server.stop(0);
+}
+
 
     public static String GetMessage() {
         return "hello MaintenanceMonitor";
